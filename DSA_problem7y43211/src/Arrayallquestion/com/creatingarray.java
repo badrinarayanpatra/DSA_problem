02d3fb0312601d79541cc9173d0e5f813173a2b6
BADRI NARAYAN PATRA <BADRI NARAYAN PATRA@192.168.1.9>
2026-06-27 15:53:59 +0530

    first

--- DSA_problem7y43211/src/Arrayallquestion/com/creatingarray.java
+++ DSA_problem7y43211/src/Arrayallquestion/com/creatingarray.java
@@ -3,7 +3,28 @@ package Arrayallquestion.com;
 public class creatingarray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//first way to create array
+	int arr1[]= {1,2,3,4,4};
+	for(int arr:arr1) {
+		System.out.print(arr+" ");
+	
+	}
+	System.out.println();
+	
+	//2nd way to create array
+	int arr2[]=new int[4];
+	arr2[0]=10;
+	arr2[1]=20;
+	arr2[2]=30;
+	arr2[3]=40;
+	
+	for(int arrr:arr2) {
+		System.out.print(arrr+" ");
+	}
+	
+	
+	
 
 	}
 
