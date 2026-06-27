56d90a260545debfec970f6def4bec7248b323e0
BADRI NARAYAN PATRA <BADRI NARAYAN PATRA@192.168.1.9>
2026-06-27 21:17:25 +0530

    array programm

---
+++ DSA_problem7y43211/src/Arrayallquestion/com/print_odd_value.java
@@ -0,0 +1,14 @@
+package Arrayallquestion.com;
+//Write a java programm to print odd index value of an array
+public class print_odd_value {
+
+	public static void main(String[] args) {
+		int arr[]= {1,2,3,4,5,6,7,8,9,10};
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]%2!=0) {
+				System.out.println(arr[i]+" ");
+			}
+		}
+	}
+
+}
